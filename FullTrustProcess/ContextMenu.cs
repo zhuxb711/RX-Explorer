7a167ed17cd4f23aa45e9a5be1a1096a695041f0
Ruofan <zrfcfgs@outlook.com>
2021-01-22 19:06:49 +0800

    Improve the code of getting context menu

--- FullTrustProcess/ContextMenu.cs
+++ FullTrustProcess/ContextMenu.cs
@@ -12,8 +12,6 @@ namespace FullTrustProcess
 {
     public static class ContextMenu
     {
-        private const int BufferSize = 512;
-
         public static List<ContextMenuPackage> FetchContextMenuItems(string Path, bool FetchExtensionMenu = false)
         {
             try
@@ -21,136 +19,73 @@ namespace FullTrustProcess
                 if (File.Exists(Path) || Directory.Exists(Path))
                 {
                     using (ShellItem Item = ShellItem.Open(Path))
+                    using (ShellContextMenu ContextMenu = new ShellContextMenu(Item))
                     {
-                        Shell32.IContextMenu Context = Item.Parent.GetChildrenUIObjects<Shell32.IContextMenu>(null, Item);
+                        List<ContextMenuPackage> ContextMenuItemList = new List<ContextMenuPackage>();
 
-                        try
+                        foreach (var MenuItem in ContextMenu.GetItems(FetchExtensionMenu ? Shell32.CMF.CMF_EXTENDEDVERBS : Shell32.CMF.CMF_NORMAL))
                         {
-                            using (User32.SafeHMENU NewMenu = User32.CreatePopupMenu())
+                            if (string.IsNullOrEmpty(MenuItem.Verb))
                             {
-                                Context.QueryContextMenu(NewMenu, 0, 0, ushort.MaxValue, FetchExtensionMenu ? (Shell32.CMF.CMF_NORMAL | Shell32.CMF.CMF_EXTENDEDVERBS) : Shell32.CMF.CMF_NORMAL);
+                                continue;
+                            }
 
-                                int MaxCount = User32.GetMenuItemCount(NewMenu);
-
-                                List<ContextMenuPackage> ContextMenuItemList = new List<ContextMenuPackage>(MaxCount);
-
-                                for (uint i = 0; i < MaxCount; i++)
-                                {
-                                    IntPtr DataPtr = Marshal.AllocHGlobal(BufferSize);
-
-                                    try
+                            switch (MenuItem.Verb.ToLower())
+                            {
+                                case "open":
+                                case "opennewprocess":
+                                case "pintohome":
+                                case "cut":
+                                case "copy":
+                                case "paste":
+                                case "delete":
+                                case "properties":
+                                case "openas":
+                                case "link":
+                                case "runas":
+                                case "rename":
+                                case "{e82bd2a8-8d63-42fd-b1ae-d364c201d8a7}":
                                     {
-                                        User32.MENUITEMINFO Info = new User32.MENUITEMINFO
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        if (!string.IsNullOrEmpty(MenuItem.HelpText))
                                         {
-                                            fMask = User32.MenuItemInfoMask.MIIM_STRING | User32.MenuItemInfoMask.MIIM_ID | User32.MenuItemInfoMask.MIIM_FTYPE | User32.MenuItemInfoMask.MIIM_BITMAP | User32.MenuItemInfoMask.MIIM_STATE,
-                                            dwTypeData = DataPtr,
-                                            cch = Convert.ToUInt32(BufferSize - 1),
-                                            cbSize = Convert.ToUInt32(Marshal.SizeOf(typeof(User32.MENUITEMINFO)))
-                                        };
-
-                                        if (User32.GetMenuItemInfo(NewMenu, i, true, ref Info))
-                                        {
-                                            if (Info.fType == User32.MenuItemType.MFT_STRING)
+                                            if (MenuItem.BitmapHandle != HBITMAP.NULL)
                                             {
-                                                IntPtr VerbPtr = Marshal.AllocHGlobal(BufferSize);
-
-                                                try
+                                                using (Bitmap OriginBitmap = MenuItem.BitmapHandle.ToBitmap())
                                                 {
-                                                    Context.GetCommandString(new IntPtr(Info.wID), Shell32.GCS.GCS_VERBW, IntPtr.Zero, VerbPtr, Convert.ToUInt32(BufferSize - 1));
+                                                    BitmapData OriginData = OriginBitmap.LockBits(new Rectangle(0, 0, OriginBitmap.Width, OriginBitmap.Height), ImageLockMode.ReadOnly, OriginBitmap.PixelFormat);
 
-                                                    string Verb = Marshal.PtrToStringUni(VerbPtr);
-
-                                                    switch (Verb.ToLower())
+                                                    try
                                                     {
-                                                        case "open":
-                                                        case "opennewprocess":
-                                                        case "pintohome":
-                                                        case "cut":
-                                                        case "copy":
-                                                        case "paste":
-                                                        case "delete":
-                                                        case "properties":
-                                                        case "openas":
-                                                        case "link":
-                                                        case "runas":
-                                                        case "rename":
-                                                        case "{e82bd2a8-8d63-42fd-b1ae-d364c201d8a7}":
-                                                            {
-                                                                break;
-                                                            }
-                                                        default:
-                                                            {
-                                                                IntPtr HelpTextPtr = Marshal.AllocHGlobal(BufferSize);
+                                                        using (Bitmap ArgbBitmap = new Bitmap(OriginBitmap.Width, OriginBitmap.Height, OriginData.Stride, PixelFormat.Format32bppArgb, OriginData.Scan0))
+                                                        using (MemoryStream Stream = new MemoryStream())
+                                                        {
+                                                            ArgbBitmap.Save(Stream, ImageFormat.Png);
 
-                                                                try
-                                                                {
-                                                                    Context.GetCommandString(new IntPtr(Info.wID), Shell32.GCS.GCS_HELPTEXTW, IntPtr.Zero, HelpTextPtr, Convert.ToUInt32(BufferSize - 1));
-
-                                                                    string HelpText = Marshal.PtrToStringUni(HelpTextPtr);
-
-                                                                    if(!string.IsNullOrEmpty(HelpText))
-                                                                    {
-                                                                        if (Info.hbmpItem != HBITMAP.NULL)
-                                                                        {
-                                                                            using (Bitmap OriginBitmap = Info.hbmpItem.ToBitmap())
-                                                                            {
-                                                                                BitmapData OriginData = OriginBitmap.LockBits(new Rectangle(0, 0, OriginBitmap.Width, OriginBitmap.Height), ImageLockMode.ReadOnly, OriginBitmap.PixelFormat);
-
-                                                                                try
-                                                                                {
-                                                                                    using (Bitmap ArgbBitmap = new Bitmap(OriginBitmap.Width, OriginBitmap.Height, OriginData.Stride, PixelFormat.Format32bppArgb, OriginData.Scan0))
-                                                                                    using (MemoryStream Stream = new MemoryStream())
-                                                                                    {
-                                                                                        ArgbBitmap.Save(Stream, ImageFormat.Png);
-
-                                                                                        ContextMenuItemList.Add(new ContextMenuPackage(HelpText, Verb, Stream.ToArray()));
-                                                                                    }
-                                                                                }
-                                                                                finally
-                                                                                {
-                                                                                    OriginBitmap.UnlockBits(OriginData);
-                                                                                }
-                                                                            }
-                                                                        }
-                                                                        else
-                                                                        {
-                                                                            ContextMenuItemList.Add(new ContextMenuPackage(HelpText, Verb, Array.Empty<byte>()));
-                                                                        }
-                                                                    }
-                                                                }
-                                                                finally
-                                                                {
-                                                                    Marshal.FreeHGlobal(HelpTextPtr);
-                                                                }
-
-                                                                break;
-                                                            }
+                                                            ContextMenuItemList.Add(new ContextMenuPackage(MenuItem.HelpText, MenuItem.Verb, Stream.ToArray()));
+                                                        }
+                                                    }
+                                                    finally
+                                                    {
+                                                        OriginBitmap.UnlockBits(OriginData);
                                                     }
                                                 }
-                                                catch
-                                                {
-                                                    continue;
-                                                }
-                                                finally
-                                                {
-                                                    Marshal.FreeHGlobal(VerbPtr);
-                                                }
+                                            }
+                                            else
+                                            {
+                                                ContextMenuItemList.Add(new ContextMenuPackage(MenuItem.HelpText, MenuItem.Verb, Array.Empty<byte>()));
                                             }
                                         }
-                                    }
-                                    finally
-                                    {
-                                        Marshal.FreeHGlobal(DataPtr);
-                                    }
-                                }
 
-                                return ContextMenuItemList;
+                                        break;
+                                    }
                             }
                         }
-                        finally
-                        {
-                            Marshal.ReleaseComObject(Context);
-                        }
+
+                        return ContextMenuItemList;
                     }
                 }
                 else
@@ -171,6 +106,7 @@ namespace FullTrustProcess
                 if (File.Exists(Path) || Directory.Exists(Path))
                 {
                     using (ShellItem Item = ShellItem.Open(Path))
+                    using (ShellContextMenu ContextMenu = new ShellContextMenu(Item))
                     {
                         Shell32.CMINVOKECOMMANDINFOEX InvokeCommand = new Shell32.CMINVOKECOMMANDINFOEX
                         {
@@ -179,23 +115,10 @@ namespace FullTrustProcess
                             cbSize = Convert.ToUInt32(Marshal.SizeOf(typeof(Shell32.CMINVOKECOMMANDINFOEX)))
                         };
 
-                        Shell32.IContextMenu Context = Item.GetHandler<Shell32.IContextMenu>(Shell32.BHID.BHID_SFUIObject);
-
                         using (User32.SafeHMENU NewMenu = User32.CreatePopupMenu())
                         {
-                            try
-                            {
-                                Context.QueryContextMenu(NewMenu, 0, 0, ushort.MaxValue, Shell32.CMF.CMF_NORMAL | Shell32.CMF.CMF_EXTENDEDVERBS);
-                                Context.InvokeCommand(InvokeCommand);
-                            }
-                            catch
-                            {
-                                return false;
-                            }
-                            finally
-                            {
-                                Marshal.ReleaseComObject(Context);
-                            }
+                            ContextMenu.ComInterface.QueryContextMenu(NewMenu, 0, 0, ushort.MaxValue, Shell32.CMF.CMF_EXTENDEDVERBS);
+                            ContextMenu.ComInterface.InvokeCommand(InvokeCommand);
                         }
                     }
 
